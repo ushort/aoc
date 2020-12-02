ff02f3c6b258a5705a3c0ec6858f948af1b58aaa
Chris <ushort@hotmail.com>
2020-12-02 17:45:25 -0500

    Solve day 1 puzzle

---
+++ src/main/kotlin/me/chris/aoc2020/Day1.kt
@@ -0,0 +1,24 @@
+package me.chris.aoc2020
+
+import java.lang.IllegalArgumentException
+import java.nio.file.Files
+import java.nio.file.Paths
+
+/**
+ * @author Christopher
+ * @since 12/2/2020
+ */
+fun main(args: Array<String>) {
+    check(args.size == 1) { "Enter a target sum." }
+    val targetSum = args[0].toInt()
+    val inputPath = Paths.get(ClassLoader.getSystemResource("input1.txt").toURI())
+    val expenses = Files.readAllLines(inputPath).toHashSet().map { s -> s.toInt() }
+    for (expense in expenses) {
+        val second = targetSum - expense
+        if (expenses.contains(second)) {
+            println("The two numbers with a sum of $targetSum multiplied together is ${expense * second}.")
+            return
+        }
+    }
+    throw IllegalArgumentException("Could not find two numbers which sum is $targetSum.")
+}
