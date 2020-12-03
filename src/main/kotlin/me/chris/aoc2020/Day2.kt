46eafec03ba4791468d223ebcb056244463fd094
Chris <ushort@hotmail.com>
2020-12-03 14:11:36 -0500

    Clean up Day 2 code

--- src/main/kotlin/me/chris/aoc2020/Day2.kt
+++ src/main/kotlin/me/chris/aoc2020/Day2.kt
@@ -9,15 +9,14 @@ import java.nio.file.Paths
  */
 fun main() {
     val inputPath = Paths.get(ClassLoader.getSystemResource("input2.txt").toURI())
-    val passwords = Files.readAllLines(inputPath).toHashSet().sorted().toList()
+    val passwords = Files.readAllLines(inputPath)
     println(passwords.filter(::isValid).size)
 }
 
 fun isValid(policyAndPassword: String): Boolean {
     val min = policyAndPassword.substringBefore("-").toInt()
     val max = policyAndPassword.substringAfter("-").substringBefore(" ").toInt()
-    val range = min..max
     val requiredCharacter = policyAndPassword.substringAfter(" ").substringBefore(":").single()
     val password = policyAndPassword.substringAfter(": ")
-    return range.contains(password.filter { it == requiredCharacter }.length)
+    return (min..max).contains(password.filter { it == requiredCharacter }.length)
 }
